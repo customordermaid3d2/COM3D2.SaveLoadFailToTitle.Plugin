71e31e62ea6d1f233f7e76a077adaa6319df21ec
lilly <lilly1987@naver.com>
2021-06-13 00:15:59 +0900

    초기 커밋.

---
+++ BepInPluginSample/SaveLoadFailToTitle.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInPluginSample;
+using COM3D2.Lilly.Plugin;
+using COM3D2API;
+using HarmonyLib;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace COM3D2.SaveLoadFailToTitle.Plugin
+{
+    [BepInPlugin(MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_FULL_NAME, MyAttribute.PLAGIN_VERSION)]// 버전 규칙 잇음. 반드시 2~4개의 숫자구성으로 해야함. 미준수시 못읽어들임
+    public class SaveLoadFailToTitle : BaseUnityPlugin
+    {
+
+        Harmony harmony;
+
+        public void OnEnable()
+        {
+            harmony = Harmony.CreateAndPatchAll(typeof(SaveLoadFailToTitlePatch));
+        }
+        public void OnDisable()
+        {
+            harmony.UnpatchSelf();
+        }
+
+    }
+}
---
+++ BepInPluginSample/SaveLoadFailToTitlePatch.cs
@@ -0,0 +1,22 @@
+﻿using COM3D2.Lilly.Plugin;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BepInPluginSample
+{
+    class SaveLoadFailToTitlePatch
+    {
+        [HarmonyPatch(typeof(GameMain), "Deserialize", new Type[] { typeof(int), typeof(bool) })]
+        [HarmonyPostfix]
+        public static void Deserialize(ref bool __result)
+        {
+            if (!__result)
+            {
+                GameMain.Instance.LoadScene("SceneToTitle");
+            }
+        }
+    }
+}
